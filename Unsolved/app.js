1a12a9b8b71ad8c7b17754d1ac3655ec351f3c22
CPDEZ08 <carlosalbertomachadopires@gmail.comm>
2021-01-31 18:32:08 -0600

    adding files

--- Unsolved/app.js
+++ Unsolved/app.js
@@ -6,7 +6,7 @@ const svgWidth = 300
 
 const rectY = 5
 const rectWidth = 20
-const rectPAdding = 5
+const rectPadding = 5
 
 // Upside Down
 let svg =d3.select("#svg-area")
@@ -20,7 +20,7 @@ svg.selectAll("rect")
     .append("rect")
     .attr("fill", "#FF00FF")
     .attr("stroke", "#000000")
-    .attr("x", (d, i) => rectPAdding + (rectWidth + rectPAdding) * i)
+    .attr("x", (d, i) => rectPadding + (rectWidth + rectPadding) * i)
     .attr("y", rectY)
     .attr("height", d => d * 10)
     .attr("width", rectWidth)
@@ -44,14 +44,37 @@ svgVert.selectAll("rect")
     .append("rect")
     .attr("fill", "#FF00FF")
     .attr("stroke", "#000000")
-    .attr("x", (d, i) => rectPAdding + (rectWidth + rectPAdding) * i)
-    .attr("y", rectY)
+    .attr("x", (d, i) => rectPadding + (rectWidth + rectPadding) * i)
+    .attr("y", d => svgHeight - (d * 10) - rectPadding)
     .attr("height", d => d * 10)
     .attr("width", rectWidth)
 
 // BONUS
 // Horizontal Bar Chart
 // YOUR CODE HERE
+
+const svgWidthHor = 600
+const svgHeightHor = 300
+
+const rectHeightHor = 20
+const rectPaddingHor = 5
+
+let svgHor =d3.select("#svg-area")
+    .append("svg")
+    .attr("height", svgHeight)
+    .attr("width", svgWidth)
+
+svgHor.selectAll("rect")
+    .data(booksReadThisYear)
+    .enter()
+    .append("rect")
+    .attr("fill", "#FF00FF")
+    .attr("stroke", "#000000")
+    .attr("x", rectPaddingHor)
+    .attr("y", (d, i) => rectPaddingHor + (rectHeightHor + rectPaddingHor) * i)
+    .attr("height", rectHeightHor)
+    .attr("width", d => d * 10)
+
 
 // BONUS 2
 // Alter your Vertical bar chart code to go from bottom  up.
